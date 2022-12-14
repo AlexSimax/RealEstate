65aba2a34be6d159da29e8017ec2a4a0564a3d9e
Alex Simax <eurock3@gmail.com>
2022-12-14 19:54:29 +0300

    footer add links

--- comps/Footer.jsx
+++ comps/Footer.jsx
@@ -1,6 +1,7 @@
 import { Box, Text, Flex, Spacer } from '@chakra-ui/react'
 import { FaPhoneAlt, FaWhatsapp } from 'react-icons/fa'
 import { FiMail, FiPhone } from 'react-icons/fi'
+import Link from 'next/link'
 
 const Footer = () => (
   <Box marginTop='20px' borderTop='1px' borderColor='gray.100'>
@@ -9,12 +10,21 @@ const Footer = () => (
         <p>Copyright {new Date().getFullYear()}, QWERTY</p>
       </Box>
       <Flex>
-        <Box color='gray.600' paddingTop={1} paddingRight={3}>
-          <FiPhone />
-        </Box>
-        <Box paddingTop={1} paddingRight={3}>
-          <FaWhatsapp color='#7ad06d' />
-        </Box>
+        <Link href='tel:+905338776057'>
+          <Box
+            color='gray.600'
+            paddingTop={1}
+            paddingRight={3}
+            cursor='pointer'
+          >
+            <FiPhone />
+          </Box>
+        </Link>
+        <Link href='https://wa.me/+905338776057'>
+          <Box paddingTop={1} paddingRight={3} cursor='pointer'>
+            <FaWhatsapp color='#7ad06d' />
+          </Box>
+        </Link>
         <Box color='brandOrange.100'>+90 (533) 877 6057</Box>
       </Flex>
       <Flex color='gray.600'>
